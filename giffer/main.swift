608fe9fed9f651d7a0669e50be5695b390b823a6
Joe Smith <jsmith@twitter.com>
2014-12-22 15:55:00 +0800

    Initial Commit

---
+++ giffer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  giffer
+//
+//  Created by Joseph Smith on 1/9/15.
+//  Copyright (c) 2015 Joseph Smith. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
